266b2abe46a543b6bff9ca148e40ce4f0c618f56
Jelena Feliciano <jfel217@gmail.com>
2021-04-07 21:18:27 -0400

    update
    created a damageShip function with some unit testing

--- test/ship_test.js
+++ test/ship_test.js
@@ -65,3 +65,18 @@ describe('checkForShip', function () {
 
     });
 });
+
+describe('damageShip', function() {
+    let damageShip = require('../game_logic/ship_methods').damageShip;
+
+    it('should register damage on a given ship at a given location', function() {
+        let ship = {
+            location: [[0, 0]],
+            damage: []
+        };
+        damageShip(ship, [0, 0]);
+
+        expect(ship.damage).to.not.be.empty;
+        expect(ship.damage[0]).to.deep.equal([0, 0]);
+    });
+});
